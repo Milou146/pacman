f6723bcf9c9f444dbac1e05004413a6671470243
sinaize <102658807+sinaize@users.noreply.github.com>
2022-04-05 12:47:53 +0200

    Update Super.java

--- src/Super.java
+++ src/Super.java
@@ -1,3 +1,32 @@
-public class Super {
-    
+public class Super extends PacGomme{
+
+    /**
+     * La durée de la super Pac-gomme
+     */
+    public int duree = 4;
+
+    /**
+     * Une super pac-gomme
+     * 
+     * @param ref
+     * @param type
+     * @param nbPoints
+     */
+    public Super(int ref, String type, int nbPoints) {
+        super(ref, type, nbPoints);
+    }
+
+    /**
+     * Permet d'implémenter l'invincibilité de Pacman.
+     * inclus:
+     * -changement de l'état des phantomes à 1:en fuite
+     * -changement de l'état de Pacman à invincible(clignote)
+     * -changement de la vitesse des phantomes 
+     */
+    public void invincibilite(){
+        Ghost.etatG = 1;
+        Pacman.etatP = 1;
+        Ghost.changeV();
+    }
+
 }
