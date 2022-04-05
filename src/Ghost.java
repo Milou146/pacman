e9c3a19fd12967fcd5df9e7eadd0a900dec6220c
sinaize <102658807+sinaize@users.noreply.github.com>
2022-04-05 12:59:36 +0200

    Update Ghost.java

--- src/Ghost.java
+++ src/Ghost.java
@@ -1,3 +1,22 @@
 public class Ghost {
     
+    public int etatG;
+
+    public int vitesse;
+
+    public void moveG(){
+
+    }
+
+    public void changeV(){
+
+    }
+
+    public int getEtatG(){
+
+    }
+
+    public String getMoveG(){
+        
+    }
 }
