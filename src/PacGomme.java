36e553c6abbbf51b69ace493a849e5b040842166
sinaize <102658807+sinaize@users.noreply.github.com>
2022-04-05 12:13:36 +0200

    Update PacGomme.java

--- src/PacGomme.java
+++ src/PacGomme.java
@@ -1,3 +1,45 @@
-public class PacGomme {
+public class PacGomme extends Entite{
     
+    /**
+     * Le type de Pac-gomme, soit son nom
+     */
+    public String type;
+
+    /**
+     * le nombre de points d'une Pac-gomme, dépendant de son type:
+     * normal:10 pt,
+     * cerise:100 pt,
+     * fraise:300 pt,
+     * orange:500 pt,
+     * pomme:700 pt,
+     * melon:1000 pt,
+     * galbosse:2000 pt,
+     * cloche:3000 pt,
+     * cle:5000 pt,
+     * super:50 pt
+     */
+    public int nbPoints;
+
+    /**
+     * Le nombre de Pac-gommes mangées par Pacman dans le niveau
+     */
+    public int nbManger;
+
+    /**
+     * Le nombre de Pac-gommes dans le niveau
+     */
+    public int nb;
+
+    /**
+     * Permet de commaitre le nombre de points d'une Pac-gomme
+     * 
+     * @return le nombre de points de la Pac-gomme
+     */
+    public int getPointPG(){
+        return this.nbPoints;
+    }
+
+    public int decPG(){
+        return nb-1;
+    }
 }
