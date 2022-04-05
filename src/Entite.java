c7abaf970f706acd201405d0670cd82754a4ed06
sinaize <102658807+sinaize@users.noreply.github.com>
2022-04-05 12:03:44 +0200

    Update Entite.java

--- src/Entite.java
+++ src/Entite.java
@@ -1,3 +1,21 @@
 public class Entite {
     
+    /**
+     * La référence de l'entité:
+     * 1-> Pacman
+     * 2-> Ghost
+     * 3-> Point
+     * 4x-> Pac-gomme
+     * p-> mur
+     */
+    public int ref;
+
+    /**
+     * La référence
+     * 
+     * @return la référence
+     */
+    public int getRef(){
+        return this.ref;
+    }
 }
